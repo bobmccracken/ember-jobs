a594e5040692744ebc2e31ff63266daf478d72ef
Chinthaka Sajith <sajithdoo@gmail.com>
2015-04-20 17:33:57 +0530

    Commit message

--- app/pods/job-form/component.js
+++ app/pods/job-form/component.js
@@ -6,21 +6,24 @@ export default Ember.Component.extend({
     this._super(...arguments);
     this.set('job', {});
   },
-
+  selectedCompany:null,
   actions: {
     async save() {
       var jobAttributes = this.get('job');
-
+      
       this.set('isSaving', true);
 
       try {
         await this.store.createRecord('job', jobAttributes).save();
+//        await this.store.createRecord('company', companyAttributes).save();
+//        await this.store.createRecord('location', locationAttributes).save();
       } finally {
         this.set('isSaving', false);
         this.sendAction('jobCreated');
       }
     }
-  },
+  }
+  ,
   location:function(){
       return this.store.findAll('location');                              
   }.property(),
