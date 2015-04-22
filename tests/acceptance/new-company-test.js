a1494a59afe88812f3adcf60393f8b2dd8a00a87
Chinthaka Sajith <sajithdoo@gmail.com>
2015-04-22 16:49:52 +0530

    file input logo testing

--- tests/acceptance/new-company-test.js
+++ tests/acceptance/new-company-test.js
@@ -3,35 +3,41 @@ import startApp from '../helpers/start-app';
 import Pretender from 'pretender';
 import json from '../helpers/json';
 import text from '../helpers/text';
-import { module, test } from 'qunit';
-import { stubResolver } from '../helpers/container';
+import {
+    module, test
+}
+from 'qunit';
+import {
+    stubResolver
+}
+from '../helpers/container';
 
 var application, server;
 
 module('Acceptance: NewCompany', {
-  beforeEach() {
-    server = new Pretender();
-    application = startApp({ }, function(app) {
-      stubResolver(app, 'adapter:application', DS.RESTAdapter);
-    });
-  },
+    beforeEach() {
+            server = new Pretender();
+            application = startApp({}, function (app) {
+                stubResolver(app, 'adapter:application', DS.RESTAdapter);
+            });
+        },
 
-  afterEach() {
-    server.shutdown();
+        afterEach() {
+            server.shutdown();
 
-    Ember.run(application, 'destroy');
-  }
+            Ember.run(application, 'destroy');
+        }
 });
 
 function isVisible(assert, selector) {
-  assert.ok($(selector).is(':visible'), 'expected `' + selector + '` to be visible');
+    assert.ok($(selector).is(':visible'), 'expected `' + selector + '` to be visible');
 }
 
 function isNotVisible(assert, selector) {
-  assert.ok(!$(selector).is(':visible'), 'expected `' + selector + '` to NOT be visible');
+    assert.ok(!$(selector).is(':visible'), 'expected `' + selector + '` to NOT be visible');
 }
 
-test('visiting / and opening/closing the  new company modal', async function test(assert) {
+test('visiting / and opening/closing the  new job modal', async function test(assert) {
   server.get('/jobs',      json(200, { jobs:      [] }));
   server.get('/companies', json(200, { companies: [] }));
   server.get('/locations', json(200, { locations: [] }));
@@ -50,32 +56,38 @@ test('form data is successfully passed to server', async function test(assert) {
   server.get('/jobs',      json(200, { jobs:      [] }));
   server.get('/companies', json(200, { companies: [] }));
   server.get('/locations', json(200, { locations: [] }));
-  
-   
-  var companyObj = {
-    name: 'test company'
-    
-  }
 
-  server.post('/companies', function(request) {
-    var requestLocation = JSON.parse(request.requestBody).company;
-    
-    assert.equal(requestLocation.name, companyObj.name);
-   
-    
-    
-    return [201, {"Content-Type": "application/json"}, JSON.stringify(companyObj)];
-  });
 
-  await visit('/');
-  await click('#create-company');
-  isVisible(assert, '.new-company-modal');
-  
-  await fillIn('input#name', companyObj.name);
-  
+        var companyObj = {
+            name: 'test company',
+            logo:'',
+            url:'bcs@uor'
 
-  await click('#save');
-  
-  isNotVisible(assert, '.new-company-modal');
+        }
 
-});
+        server.post('/companies', function (request) {
+            var requestLocation = JSON.parse(request.requestBody).company;
+
+            assert.equal(requestLocation.name, companyObj.name);
+            assert.equal(requestLocation.logo, companyObj.logo);
+            assert.equal(requestLocation.url, companyObj.url);
+
+
+            return [201, {
+                "Content-Type": "application/json"
+            }, JSON.stringify(companyObj)];
+        });
+
+        await visit('/');
+        await click('#create-company');
+        isVisible(assert, '.new-company-modal');
+        var bgimage=window.location.pathname;
+        //alert(bgimage);
+        await fillIn('input#name', companyObj.name);
+        await fillIn('input#logo', bgimage.pathname);
+        await fillIn('input#url', companyObj.url);
+        await click('#save');
+
+        isNotVisible(assert, '.new-company-modal');
+
+    });
